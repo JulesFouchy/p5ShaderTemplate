1548ac71f978102af6e4bded08954b6276805775
Jules Fouchy <jules.fouchy@ntymail.com>
2020-10-09 13:28:44 +0200

    Properly center the canvas when it's not fullscreen ; refactor the choice of canvas size

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,3 +1,4 @@
+let canvas: p5.Renderer // Need to access this each time we resize the window, to center the canvas
 let myShader: p5.Shader
 
 function preload() {
@@ -5,15 +6,37 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight, WEBGL)
+    canvas = createCanvas(desiredCanvasWidth(), desiredCanvasHeight(), WEBGL)
+    canvas.position((windowWidth - width)/2, (windowHeight - height)/2) // Center the canvas
 }
 
 function draw() {
     shader(myShader)
     myShader.setUniform("uAspectRatio", width / height)
-    rect(0, 0, width, height)
+    rect(0, 0, 0, 0)
 }
 
+
+// -------------------
+// Choose how your canvas will fit in the window
+// -------------------
+// Fill the window
+/*function desiredCanvasWidth(): number {
+    return windowWidth;
+}
+function desiredCanvasHeight(): number {
+    return windowHeight;
+}*/
+// Squared canvas with a margin
+function desiredCanvasWidth(): number {
+    const margin = 25 // in pixels
+    return min(windowWidth, windowHeight) - margin * 2;
+}
+function desiredCanvasHeight(): number {
+    return desiredCanvasWidth();
+}
+// -------------------
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight)
+    resizeCanvas(desiredCanvasWidth(), desiredCanvasHeight())
+    canvas.position((windowWidth - width)/2, (windowHeight - height)/2) // Center the canvas
 }
