97e5bc517670d85683ec5b97e69b8d5488f5ec21
Jules Fouchy <jules.fouchy@ntymail.com>
2021-02-02 12:49:43 +0100

    Actually prefix internal p6 things with "__" to free the p6 "namespace"

--- build/build.js
+++ build/build.js
@@ -1,35 +1,3 @@
-var ASPECT_RATIO = 1;
-var MARGIN_SIZE = 25;
-function desiredCanvasWidth() {
-    var windowRatio = windowWidth / windowHeight;
-    if (ASPECT_RATIO > windowRatio) {
-        return windowWidth - MARGIN_SIZE * 2;
-    }
-    else {
-        return desiredCanvasHeight() * ASPECT_RATIO;
-    }
-}
-function desiredCanvasHeight() {
-    var windowRatio = windowWidth / windowHeight;
-    if (ASPECT_RATIO > windowRatio) {
-        return desiredCanvasWidth() / ASPECT_RATIO;
-    }
-    else {
-        return windowHeight - MARGIN_SIZE * 2;
-    }
-}
-var canvas;
-function _centerCanvas() {
-    canvas.position((windowWidth - width) / 2, (windowHeight - height) / 2);
-}
-function template_CreateCanvas() {
-    canvas = createCanvas(desiredCanvasWidth(), desiredCanvasHeight(), WEBGL);
-    _centerCanvas();
-}
-function template_ResizeCanvas() {
-    resizeCanvas(desiredCanvasWidth(), desiredCanvasHeight());
-    _centerCanvas();
-}
 var myShader;
 var gui = new dat.GUI();
 var params = {
@@ -50,9 +18,60 @@ function preload() {
     myShader = loadShader("shader/myShader.vert", "shader/myShader.frag");
 }
 function setup() {
-    template_CreateCanvas();
+    p6_CreateCanvas();
 }
 function windowResized() {
-    template_ResizeCanvas();
+    p6_ResizeCanvas();
 }
+var __ASPECT_RATIO = 1;
+var __MARGIN_SIZE = 25;
+function __desiredCanvasWidth() {
+    var windowRatio = windowWidth / windowHeight;
+    if (__ASPECT_RATIO > windowRatio) {
+        return windowWidth - __MARGIN_SIZE * 2;
+    }
+    else {
+        return __desiredCanvasHeight() * __ASPECT_RATIO;
+    }
+}
+function __desiredCanvasHeight() {
+    var windowRatio = windowWidth / windowHeight;
+    if (__ASPECT_RATIO > windowRatio) {
+        return __desiredCanvasWidth() / __ASPECT_RATIO;
+    }
+    else {
+        return windowHeight - __MARGIN_SIZE * 2;
+    }
+}
+var __canvas;
+function __centerCanvas() {
+    __canvas.position((windowWidth - width) / 2, (windowHeight - height) / 2);
+}
+function p6_CreateCanvas() {
+    __canvas = createCanvas(__desiredCanvasWidth(), __desiredCanvasHeight(), WEBGL);
+    __centerCanvas();
+}
+function p6_ResizeCanvas() {
+    resizeCanvas(__desiredCanvasWidth(), __desiredCanvasHeight());
+    __centerCanvas();
+}
+var p6_SaveImageSequence = function (durationInFrames, fileExtension) {
+    if (frameCount <= durationInFrames) {
+        noLoop();
+        var filename_1 = nf(frameCount - 1, ceil(log(durationInFrames) / log(10)));
+        var mimeType = (function () {
+            switch (fileExtension) {
+                case 'png':
+                    return 'image/png';
+                case 'jpeg':
+                case 'jpg':
+                    return 'image/jpeg';
+            }
+        })();
+        canvas.elt.toBlob(function (blob) {
+            p5.prototype.downloadFile(blob, filename_1, fileExtension);
+            setTimeout(function () { return loop(); }, 100);
+        }, mimeType);
+    }
+};
 //# sourceMappingURL=../src/src/build.js.map
